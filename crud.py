5f4d557a11a749a4edc7ddf2b9e60646a2d29ae5
Shirley Crenshaw <crenshs@gmail.com>
2022-10-19 20:33:13 -0400

    finished part 3

--- crud.py
+++ crud.py
@@ -6,19 +6,27 @@ def create_user(email, password):
 
     user = User(email=email, password=password)
 
-    return user
+    return user    
 
 def create_movie(title, overview, release_date, poster_path):
     """Create and return a new movie."""
 
     movie = Movie(
         title=title,
-        overview=overview
+        overview=overview,
         release_date=release_date,
         poster_path=poster_path,
-    c)
+    )
     
     return movie
+
+def get_movies():
+    """Return all movies"""
+    return Movie.query.all()
+
+def get_movie_by_id(movie_id):
+    """Get movie by movie_id"""
+    return Movie.query.get(movie_id)
 
 def create_rating(user, movie, score):
     """Create and return a new rating."""
@@ -26,6 +34,14 @@ def create_rating(user, movie, score):
     rating = Rating(user=user, movie=movie, score=score)
 
     return rating
+    
+def get_users():
+    """Show user email and link profile"""
+    return User.query.all()
+
+def get_user_by_email(email):
+    return User.query.get(email)
+
 
 if __name__ == '__main__':
     from server import app
